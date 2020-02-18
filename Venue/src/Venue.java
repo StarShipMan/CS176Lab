dbfd43d6af02f7b67c46a63f51e5283d7ae14a43
MikiM <MikiM@10.71.2.225>
2020-02-18 11:29:48 -0500

    Final Commit for Version 1

--- Venue/src/Venue.java
+++ Venue/src/Venue.java
@@ -1,4 +1,59 @@
-
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Random;
 public class Venue {
-
-}
+	Random seatsTakenGenerator = new Random();
+	final int INITIAL_SEATS_TAKEN = 100;
+	public boolean[][][] allSeats;
+	int sections;
+	int rows;
+	int seats;
+	int sectionTaken;
+	int rowTaken;
+	int seatTaken;
+	int generatorCounter = 0;
+	
+	public Venue(int inSection, int inRow, int inSeat) {
+		sections = inSection;
+		rows = inRow;
+		seats = inSeat;
+		allSeats = new boolean [inSection][inRow][inSeat];
+		for (int section = 0; section < inSection; section++) {
+			for (int row = 0; row < inRow; row++) {
+				for (int seat = 0; seat < inSeat; seat++) {
+					allSeats[section][row][seat] = false;
+				}//for seats
+			}// for rows
+		}// for sections
+		while (generatorCounter < INITIAL_SEATS_TAKEN) {
+			sectionTaken = seatsTakenGenerator.nextInt(inSection);
+			rowTaken =   seatsTakenGenerator.nextInt(inRow);
+			seatTaken =  seatsTakenGenerator.nextInt(inSeat);
+			if (allSeats[sectionTaken][rowTaken][seatTaken]== false) {	
+				allSeats[sectionTaken][rowTaken][seatTaken] = true;
+				System.out.println(sectionTaken + "-" + rowTaken + "-" + seatTaken + ": index values------- actual values :" + (sectionTaken + 1)+ "-" + (rowTaken+1) + "-" + (seatTaken+1));
+				generatorCounter++;
+				System.out.println("generator counter: + " + generatorCounter + ",");
+			}//if seat in row and section is not already taken
+		}
+	}//Venue constructor
+	
+	public void printArray() {
+		for (int section = 0; section < sections; section++) {
+			for (int row = 0; row < rows; row++) {
+				for (int seat = 0; seat < seats; seat++) {
+					System.out.println(allSeats[section][row][seat]);
+				}
+			}
+		}
+	}
+	public String seatLookup(int inSection, int inRow, int inSeat) {
+		String awarded;
+		if (allSeats[inSection-1][inRow-1][inSeat-1]== false)
+			awarded = " seat awarded.";
+		else
+			awarded = " seat taken.";
+		return awarded;
+	}
+ 
+}//class
--- Venue/src/VenueTester.java
+++ Venue/src/VenueTester.java
@@ -1,9 +1,36 @@
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class VenueTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
-}
+		Scanner sc = new Scanner(System.in);
+		int sections = 0;
+		int rows = 0;
+		int seats = 0;
+		int awardedSection = 0;
+		int awardedRow = 0;
+		int awardedSeat = 0;
+		String awarded;
+		System.out.println("How many sections does the venue have?");
+		sections = sc.nextInt();
+		System.out.println("How many rows does each section have?");
+		rows = sc.nextInt();
+		System.out.println("How many seats does each row have?");
+		seats = sc.nextInt();
+		Venue PrudentialCenter = new Venue(sections, rows, seats);
+		System.out.println();
+		System.out.println("Which section would you like to be awarded?");
+		awardedSection = sc.nextInt();
+		System.out.println("Which row for the section would you like to be awarded?");
+		awardedRow = sc.nextInt();
+		System.out.println("Which seat for the row would you like to be awarded?");
+		awardedSeat = sc.nextInt();
+		awarded = PrudentialCenter.seatLookup(awardedSection, awardedRow, awardedSeat);
+		System.out.println(awardedSection + "-" + awardedRow + "-" + awardedSeat + "" + awarded);
+		
+		
+	}//main
+}//class
