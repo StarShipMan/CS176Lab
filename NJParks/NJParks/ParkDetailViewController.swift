6c453827a2221b469406909198be9d9a96e55220
StarShipMan <55116516+StarShipMan@users.noreply.github.com>
2020-10-15 22:23:37 -0400

    files for cs337

---
+++ NJParks/NJParks/ParkDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ParkDetailViewController.swift
+//  NJParks
+//
+//  Created by Michael Liguori on 10/15/20.
+//
+
+import UIKit
+
+class ParkDetailViewController: UIViewController {
+    
+    var thisPark: Park?
+    
+    let myParkModel = ParkModel.sharedInstance
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
